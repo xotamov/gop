016e7d2fad7c394e3eabe5d9e4a330de9ba423d5
xotamov <xotamovazizjon997@gmail.com>
2023-06-01 17:54:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// let cars = [
+//     {
+//         model: 'Gentra',
+//         price: 16_000_000,
+//         discound: 20,
+//     },
+//     {
+//         model: 'Malibu',
+//         price: 34_000_000,
+//         discound: 0,    
+//     },
+//     {
+//         model: 'Elantra',
+//         price: 24_000_000,
+//         discound: 10,    
+//     },
+//     {
+//         model: 'Cobalt',
+//         price: 14_000_000,
+//         discound: 0,    
+//     },
+//     {
+//         model: 'Sanata',
+//         price: 44_000_000,
+//         discound: 0,    
+//     },
+//     {
+//         model: 'Santafe',
+//         price: 54_000_000,
+//         discound: 0,    
+//     },
+// ]
+// let cheap = cars.filter(item =>{
+//     if(item.price <= 35_000_000){
+//         return item
+//     }
+// })
+// console.log(cheap);
+
+
+// let index = prompt('Write any number')
+// let arr =['Aleksey','Kartoshka','Margarita','Morgenshtern','Monica']
+// if(splice() === arr){
+//     console.log('wwel');
+// } else{
+//     console.log('такого элемента у нас нет');
+// }
+ 
+// let arry =[1,2,false,'hello',24,'world',undefined,null,'error',22]
+
+// if(arry.includes(Number)){
+//     console.log('good');
+// } else{
+//     console.log('bed');
+// }
+
+let arrey = [[{a: {price:20}}][{a: {price: 35}}][{a: {price: 44}}]]
+
+let total = alert(arrey[1].price)
+
+console.log(total);
